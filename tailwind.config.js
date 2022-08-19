3677fd5c5c3363a5c2f8b64e7faeafd54e3d965a
Thomas Ledesert <thomas.ledesert@gmail.com>
2022-08-19 17:16:33 +0200

    commit initial

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  // ...
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+        orange:{
+          600 : '#FF6F43'
+        },
+        cyan: {
+          100 :  '#f8fafc',
+          200 : '#f8fafc',
+          300 : '#F8F8FF',
+          400 : '#A1A5CE',
+          500 : '#42456C',
+          600 : '#42456C',
+          700 :'#42456C',
+          800 : '#21233F'
+        },
+      },
+    },
+  },
+  content: ["./src/**/*.{html,js}"],
+  plugins: [
+    // ...
+    //require("@tailwindcss/forms"),
+    //require("@tailwindcss/aspect-ratio"),
+  ],
+};
